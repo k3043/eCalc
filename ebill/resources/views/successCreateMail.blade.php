a0922c9a0ca836907fe374013e52b2725bb92a34
k3043 <21010995@st.phenikaa-uni.edu.vn>
2024-06-02 18:16:59 +0000

    delete bill

--- ebill/resources/views/successCreateMail.blade.php
+++
@@ -1,4 +0,0 @@
-<p style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;font-weight: bold;color: palevioletred;">Xin chào {{$name}}</p>
-<p style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">Tài khoản của quý khách đã được tạo thành công!</p>
-<p style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">Mã khách hàng: <span style="color: red;">{{$code}}</span></p>
-<p style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">Quý Khách có thể tra cứu hóa đơn điện bằng mã khách hàng</p>
